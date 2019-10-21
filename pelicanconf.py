24d6a7a025dd4fb38c74f29f4d404db98cadeebf
BO <bolive84@gmail.com>
2019-10-21 14:03:39 -0400

    new blog post

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,7 @@ STATIC_PATHS = ['images']
 TIMEZONE = 'America/Toronto'
 DEFAULT_LANG = 'en'
 DEFAULT_PAGINATION = 10
-THEME = 'theme/brutalist'
+THEME = 'attila'
 # set to False for Production, True for Development
 if os.environ.get('PELICAN_ENV') == 'DEV':
     RELATIVE_URLS = True
