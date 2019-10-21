fd659133db129529951ba180f9522e7532750baf
BO <bolive84@gmail.com>
2019-10-21 16:43:21 -0400

    new blog post

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,6 +6,7 @@ import os
 AUTHOR = 'Benoit Olive'
 SITENAME = 'Benoit\'s Blog'
 HOME_COVER = 'https://cdn.pixabay.com/photo/2017/04/23/19/30/earth-2254769_960_720.jpg'
+og_image = 'https://cdn.pixabay.com/photo/2018/07/03/07/09/block-chain-3513216_960_720.jpg'
 SITEURL = 'https://Bolive84.github.io/Blog'
 PATH = 'content'
 STATIC_PATHS = ['images']
@@ -13,7 +14,6 @@ TIMEZONE = 'America/Toronto'
 DEFAULT_LANG = 'en'
 DEFAULT_PAGINATION = 10
 THEME = 'theme'
-ARTICLE_COVER = 'https://cdn.pixabay.com/photo/2018/07/03/07/09/block-chain-3513216_960_720.jpg'
 # set to False for Production, True for Development
 if os.environ.get('PELICAN_ENV') == 'DEV':
     RELATIVE_URLS = True
